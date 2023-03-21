a1e3cf5110fb11c60fef80675a565aa2795e5666
Riccardo Nestola <riccardinux@gmail.com>
2023-03-21 18:08:32 +0100

    fix: forgot-password page

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,47 +1,58 @@
+
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-4">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+<div class="container">
+
+       
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                    
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-password-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-2">Password dimenticata?</h1>
+                                        <p class="mb-4">Basta inserire il tuo indirizzo email qui sotto
+                                            e ti invieremo un link per reimpostare la tua password!</p>
+                                    </div>
+                                    <form class="user" method="POST" action="{{ route('password.email') }}"> @csrf
+                                        <div class="form-group">
+                                            <input id="email" type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Inserisci la tua mail...">
+                                                @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Invia richiesta
+                                        </button>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="{{ route('register') }}">Crea un account</a>
+                                    </div>
+                                    <div class="text-center">
+                                            <a class="small" href="{{ route('login') }}">Hai già un account? Accedi!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="mb-4 row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">Indirizzo e-mail</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="mb-4 row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Invia richiesta
-                                </button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
+
             </div>
+
         </div>
+
     </div>
-</div>
 @endsection
+
+
