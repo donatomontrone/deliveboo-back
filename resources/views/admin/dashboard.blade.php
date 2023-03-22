9b20160272d66d8e6a3f1446edbab42e57d5fc2c
donatomontrone <donato.montrone@outlook.com>
2023-03-22 12:09:50 +0100

    fix: image centered in dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -47,7 +47,7 @@
                                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                                     <h6 class="m-0 font-weight-bold text-success">Immagine Ristorante</h6>
                                 </div>
-                                <div class="card-body">
+                                <div class="card-body text-center">
                                         @if (str_starts_with($restaurant->img_path, 'http'))
                                             <img src=" {{$restaurant->img_path}} " class="img-fluid">
                                             @else
