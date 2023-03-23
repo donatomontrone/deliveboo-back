4be81e3e38c2e172b22058741e893aa412111b25
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-23 17:39:53 +0100

    Moved the array put in the seeder in a custom db folder

--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -19,44 +19,7 @@ class DishSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $dishes = [
-            [
-                'name' => 'Spaghetto allo scoglio',
-                'description' => 'Piatto cucinato dallo chef Stellato Donato Montrone',
-                'ingredients' => 'Pasta, Pesce fresco, Olio, Aglio',
-                'price' => 23.99
-            ],
-            [
-                'name' => 'Pizza Nestola',
-                'description' => 'Pizza con lievito madre, impastata personalmente da Riccardo Nestola',
-                'ingredients' => 'Farina, Lievito, Olio, Acqua, Pomodoro, Mozzarella, Würstel, Patatine',
-                'price' => 11.99
-            ],
-            [
-                'name' => 'Carne all\'Aniello',
-                'description' => 'Carne di Bovino, ammazzato personalmente dallo chef Aniello Piscopo',
-                'ingredients' => 'Carne, Origano, Olio, Sale',
-                'price' => 44.85
-            ],
-            [
-                'name' => 'CheesCake di Mimmo',
-                'description' => 'Dolce prelibato preparato con prodotti a KM0 dello Chef Mimmo',
-                'ingredients' => 'Biscotti, Panna, Limone, Philadelphia',
-                'price' => 8.99
-            ],
-            [
-                'name' => 'Spezzatino di Montrone',
-                'description' => 'Carne di Montone, ammazzato personalmente dallo chef Donato',
-                'ingredients' => 'Carne, Origano, Olio, Sale',
-                'price' => 187.65
-            ],
-            [
-                'name' => 'Salumi da Donato',
-                'description' => 'Salumi di alta qualità scelti personalmente dal salumiere Donato',
-                'ingredients' => 'Salumi vari',
-                'price' => 18.99
-            ],
-        ];
+        $dishes = config('db.dishes');
 
         foreach ($dishes as $dish) {
             $newDish = new Dish();
--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -15,49 +15,7 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        $types = [ // Array 1
-            [
-                'title' => 'Etnica',
-                'img_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVOxH50WwqY6KeT7KceoeqRv8KJOGBrL3T2Q&usqp=CAU',
-                'color' => '#FF0000'
-            ],
-            // Array 2
-            [
-                'title' => 'Nazionale',
-                'img_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-267yTxSX697NT6HXEQXDpxjD7A8sZD1lNw&usqp=CAU',
-                'color' => '#00FF00'
-            ],
-            // Array 3
-            [
-                'title' => 'Giapponese',
-                'img_path' => 'https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj0o7eT9OD9AhWRRvEDHUcpARQQ1bUFegQIDBAA&url=https%3A%2F%2Fwww.watabi.it%2Fblog%2Fcultura-giapponese%2Fcucina-giapponese%2F30-piatti-giapponesi-da-provare%2F&usg=AOvVaw0o7vxLecd47CoO4GG1IaNo',
-                'color' => '#0000FF'
-            ],
-            // Array 4
-            [
-                'title' => 'Vegan',
-                'img_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-267yTxSX697NT6HXEQXDpxjD7A8sZD1lNw&usqp=CAU',
-                'color' => '#FFFF00'
-            ],
-            // Array 5
-            [
-                'title' => 'Vegetariana',
-                'img_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-267yTxSX697NT6HXEQXDpxjD7A8sZD1lNw&usqp=CAU',
-                'color' => '#00FFFF'
-            ],
-            // Array 6
-            [
-                'title' => 'Internazionale',
-                'img_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-267yTxSX697NT6HXEQXDpxjD7A8sZD1lNw&usqp=CAU',
-                'color' => '#FF00FF'
-            ],
-            // Array 7
-            [
-                'title' => 'Regionale',
-                'img_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpzmLN3UfHpK_ZNbIzp7G7hoN8gHNkFL08aQ&usqp=CAU',
-                'color' => '#000000'
-            ]
-        ];
+        $types = config('db.types');
 
         foreach ($types as $type) {
             $newType = new Type();
