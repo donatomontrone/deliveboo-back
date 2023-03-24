a915f3fcbca2209690682fdcd4e47182971bb0f8
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-03-24 13:45:09 +0100

    Modified the RestaurantController in the Api folder

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Restaurant;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
@@ -11,19 +13,21 @@ class RestaurantController extends Controller
     protected $numOfRestaurants=10;
     
     public function index(){
+        $types = Type::all();
         $restaurants = Restaurant::with('user' , 'types')->paginate(10);
         
         return response()->json([
             'success' => true,
-            'results' => $restaurants
+            'results' => ['restaurants' => $restaurants , 'types' => $types],
         ]);
     }
 
     public function show(Restaurant $restaurant){
-        $restaurant = Restaurant::with('user', 'types')->findOrFail($restaurant->id);
+        $categories = Category::all();
+        $restaurant = Restaurant::with('user', 'types','dishes')->findOrFail($restaurant->id);
         return response()->json([
             'success' => true,
-            'results' => $restaurant
+            'results' => ['restaurant' => $restaurant , 'categories' => $categories]
         ]);
     }
 }
