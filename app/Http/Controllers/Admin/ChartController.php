ee6ca69e443e94addbf8666bed473dad9744f850
donatomontrone <donato.montrone@outlook.com>
2023-04-03 11:33:57 +0200

    fix chart error

--- app/Http/Controllers/Admin/ChartController.php
+++ app/Http/Controllers/Admin/ChartController.php
@@ -13,27 +13,21 @@ class ChartController extends Controller
 {
     public function index()
     {
-        $user = Auth::user();
+        $user_id = Auth::user()->id;
 
-        $totalPrice = Order::select(DB::raw('YEAR(orders.created_at) year, MONTH(orders.created_at) month,SUM(orders.total_price) as revenue'))
-            ->join('dish_order', 'orders.id', '=', 'dish_order.order_id')
-            ->join('dishes', 'dish_order.dish_id', '=', 'dishes.id')
-            ->join('restaurants', 'dishes.restaurant_id', '=', 'restaurants.id')
-            ->join('users', 'restaurants.user_id', '=', 'users.id')
-            ->where('users.id', $user->id)
-            ->groupBy('year', 'month')
-            ->orderBy('year', 'asc')
+        $totalOrders = Order::selectRaw('DATE_FORMAT(date, "%Y-%m") as month, COUNT(*) as total_orders')
+            ->whereHas('dishes.restaurant.user', function ($query) use ($user_id) {
+                $query->where('id', $user_id);
+            })
+            ->groupBy('month')
             ->orderBy('month', 'asc')
             ->get();
 
-        $totalOrders = Order::select(DB::raw('YEAR(orders.created_at) year, MONTH(orders.created_at) month, COUNT(*) as total_orders'))
-            ->join('dish_order', 'orders.id', '=', 'dish_order.order_id')
-            ->join('dishes', 'dish_order.dish_id', '=', 'dishes.id')
-            ->join('restaurants', 'dishes.restaurant_id', '=', 'restaurants.id')
-            ->join('users', 'restaurants.user_id', '=', 'users.id')
-            ->where('users.id', Auth::user()->id)
-            ->groupBy('year', 'month')
-            ->orderBy('year', 'asc')
+        $totalPrice = Order::selectRaw('DATE_FORMAT(date, "%Y-%m") as month, SUM(total_price) as revenue')
+            ->whereHas('dishes.restaurant.user', function ($query) use ($user_id) {
+                $query->where('id', $user_id);
+            })
+            ->groupBy('month')
             ->orderBy('month', 'asc')
             ->get();
 
@@ -41,24 +35,62 @@ class ChartController extends Controller
         $dataOrders = [];
         $dataRevenue = [];
 
-        // Loop through all months in a year
-        for ($month = 1; $month <= 12; $month++) {
-            $monthOrders = $totalOrders->where('month', $month)->first();
-            if ($monthOrders) {
-                $labels[] = Carbon::createFromDate($monthOrders->year, $monthOrders->month, 1)->format('M Y');
-                $dataOrders[] = $monthOrders->total_orders;
-            } else {
-                $labels[] = Carbon::createFromDate(null, $month, 1)->format('M Y');
-                $dataOrders[] = 0;
-            }
-
-            $monthRevenue = $totalPrice->where('month', $month)->first();
-            if ($monthRevenue) {
-                $dataRevenue[] = $monthRevenue->revenue;
-            } else {
-                $dataRevenue[] = 0;
-            }
+        foreach ($totalOrders as $order) {
+            $labels[] = Carbon::createFromFormat('Y-m', $order->month)->format('M Y');
+            $dataOrders[] = $order->total_orders;
         }
+
+        foreach ($totalPrice as $price) {
+            $dataRevenue[] = $price->revenue;
+        }
+
+        return view('admin.orders.charts.index', compact('dataOrders', 'dataRevenue', 'labels'));
+        // $user = Auth::user();
+
+        // $totalPrice = Order::select(DB::raw('YEAR(orders.date) year, MONTH(orders.date) month,SUM(orders.total_price) as revenue'))
+        //     ->join('dish_order', 'orders.id', '=', 'dish_order.order_id')
+        //     ->join('dishes', 'dish_order.dish_id', '=', 'dishes.id')
+        //     ->join('restaurants', 'dishes.restaurant_id', '=', 'restaurants.id')
+        //     ->join('users', 'restaurants.user_id', '=', 'users.id')
+        //     ->where('users.id', $user->id)
+        //     ->groupBy('year', 'month')
+        //     ->orderBy('year', 'asc')
+        //     ->orderBy('month', 'asc')
+        //     ->get();
+
+        // $totalOrders = Order::select(DB::raw('YEAR(orders.date) year, MONTH(orders.date) month, COUNT(*) as total_orders'))
+        //     ->join('dish_order', 'orders.id', '=', 'dish_order.order_id')
+        //     ->join('dishes', 'dish_order.dish_id', '=', 'dishes.id')
+        //     ->join('restaurants', 'dishes.restaurant_id', '=', 'restaurants.id')
+        //     ->join('users', 'restaurants.user_id', '=', 'users.id')
+        //     ->where('users.id', Auth::user()->id)
+        //     ->groupBy('year', 'month')
+        //     ->orderBy('year', 'asc')
+        //     ->orderBy('month', 'asc')
+        //     ->get();
+
+        // $labels = [];
+        // $dataOrders = [];
+        // $dataRevenue = [];
+
+        // // Loop through all months in a year
+        // for ($month = 1; $month <= 12; $month++) {
+        //     $monthOrders = $totalOrders->where('month', $month)->first();
+        //     if ($monthOrders) {
+        //         $labels[] = Carbon::createFromDate($monthOrders->year, $monthOrders->month, 1)->format('M Y');
+        //         $dataOrders[] = $monthOrders->total_orders;
+        //     } else {
+        //         $labels[] = Carbon::createFromDate(null, $month, 1)->format('M Y');
+        //         $dataOrders[] = 0;
+        //     }
+
+        //     $monthRevenue = $totalPrice->where('month', $month)->first();
+        //     if ($monthRevenue) {
+        //         $dataRevenue[] = $monthRevenue->revenue;
+        //     } else {
+        //         $dataRevenue[] = 0;
+        //     }
+        // }
         return view('admin.orders.charts.index', compact('dataOrders', 'dataRevenue', 'labels'));
     }
 }
@@ -68,7 +100,6 @@ class ChartController extends Controller
 // {
 //     $restaurant = auth()->user()->restaurant;
 
-//     // Statistiche per i piatti più ordinati
 //     $dishes = $restaurant->dishes()->withCount('orders')->orderBy('orders_count', 'desc')->get();
 
 //     $data1 = [
